8a8927ab7ed4c493be32c8c559217c41dfaa4d33
misostack <misostack.com@gmail.com>
2022-10-06 20:16:41 +0700

    Init project java basis

---
+++ README.md
@@ -0,0 +1,12 @@
+# Java Basis
+
+## Topics
+
+### Variables
+
+- Define
+- Primitive types
+- Type casting
+- Array
+- Conditional Statement
+
---
+++ src/main/java/javacore/net/Main.java
@@ -0,0 +1,8 @@
+package javacore.net;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+    }
+}
